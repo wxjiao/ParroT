7e1c5d9ddef24b67c3d97900643728fbeb32ba0e
wxjiao <wenxiangjiaonju@gmail.com>
2023-04-14 11:44:59 +0800

    add train_lora.sh

---
+++ train_lora.sh
@@ -0,0 +1,50 @@
+# Multi-nodes are also supported
+
+export NCCL_DEBUG=INFO
+export NCCL_SOCKET_IFNAME=eth1
+export NCCL_IB_GID_INDEX=3
+export NCCL_IB_SL=3
+export NCCL_NET_GDR_READ=1
+
+export MASTER_ADDR="${CHIEF_IP:=localhost}"
+export MASTER_PORT="${MASTER_PORT:=29500}"
+
+train_path=transformers/examples/pytorch/language-modeling/run_clm_llms.py
+model_path=<your_proj_path>/llama-7b
+model_save=<your_proj_path>/parrot-hint-lora-7b
+
+# HOST_NUM will be 1
+torchrun --nnodes $HOST_NUM --node_rank $INDEX --nproc_per_node 8 \
+    --master_addr $MASTER_ADDR --master_port $MASTER_PORT  \
+    ${train_path} \
+    --deepspeed deepspeed_config.json \
+    --model_name_or_path ${model_path} \
+    --train_file data/data_parrot_hf.json \
+    --use_lora True \
+    --lora_config lora_config.json \
+    --preprocessing_num_workers 16 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 4 \
+    --num_train_epochs 1.5 \
+    --save_strategy "steps" \
+    --save_steps 500 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 10 \
+    --block_size 512 \
+    --do_train \
+    --evaluation_strategy "no" \
+    --validation_split_percentage 0 \
+    --fp16 True \
+    --fp16_full_eval True \
+    --streaming \
+    --ddp_timeout 3600 \
+    --seed 1 \
+    --gradient_checkpointing True \
+    --output_dir ${model_save}
+
+
